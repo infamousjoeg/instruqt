a6636e7329a5724c934b54a73d7878274e761c8f
Joe Garcia <joe@joe-garcia.com>
2022-02-02 17:45:35 -0500

    Add files for secure-kubernetes-secrets track

---
+++ env/conjur-k8s/secretless/developer-env.sh
@@ -0,0 +1,7 @@
+APP_NAME=testapp-secure
+APP_NAMESPACE=testapp
+APP_SERVICE_ACCOUNT_NAME=testapp-secure-sa
+
+CONJUR_ACCOUNT="default"
+CONJUR_APPLIANCE_URL="https://conjur-cluster-conjur-oss.conjur-server.svc.cluster.local"
+AUTHENTICATOR_ID="dev"
